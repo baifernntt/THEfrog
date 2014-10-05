5d3ca218d0a9a650a18978923be4184f09ef2a2b
natcha <natcha@192.168.1.41>
2014-10-05 23:03:40 +0700

    Game Over

--- src/thefrog/Thefrog.java
+++ src/thefrog/Thefrog.java
@@ -9,10 +9,8 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
-
-
 public class Thefrog extends BasicGame {
-	
+
 	public static final int GAME_WIDTH = 640;
 	public static final int GAME_HEIGHT = 480;
 	public static final float G = (float) 0.5;
@@ -23,108 +21,108 @@ public class Thefrog extends BasicGame {
 	private boolean isStarted;
 	private boolean isGameOver;
 	private tree[] trees;
-	private boolean[] useTree = new boolean[]{false,false,false};
+	private boolean[] useTree = new boolean[] { false, false, false };
 	private Image land;
 	public static final float bg_Vy = -3;
-	private int treeCurrent=0;
+	private int treeCurrent = 0;
 	public static int checkTrees = 0;
 	long runningTime = 200;
-	
-	
+
 	public Thefrog(String title) {
 		super(title);
-		
+
 	}
 
 	@Override
-	public void render(GameContainer container, Graphics g) throws SlickException {
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
 		g.drawImage(land, 0, 0);
-		
-		for (tree Trees: trees){
+
+		for (tree Trees : trees) {
 			Trees.render();
 		}
-		
+
 		Frog.render();
 		g.drawString("score = " + Frog.score, 320, 20);
-		
-		
+
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		
-	    
-	    land = new Image("res/bg1.png");
-		Frog = new frog(GAME_WIDTH/2, 40,Frog_JUMP_VY);
+
+		land = new Image("res/bg1.png");
+		Frog = new frog(GAME_WIDTH / 2, 40, Frog_JUMP_VY);
 		isStarted = false;
 		inittrees();
 	}
 
 	private void inittrees() throws SlickException {
 		trees = new tree[3];
-	    for (int i = 0; i < 3; i++) {
-	      trees[i] = new tree(80+100*i, 120*i+150 ,Tree_VX-i);
-	    }
+		for (int i = 0; i < 3; i++) {
+			trees[i] = new tree(80 + 100 * i, 120 * i + 150, Tree_VX - i);
+		}
 	}
 
 	@Override
 	public void update(GameContainer container, int g) throws SlickException {
-		
-		if (!isGameOver){	
-			if(isStarted == true){
-					Frog.update();
-			
-				for(int i=0;i<3;i++){
+
+		if (!isGameOver) {
+			if (isStarted == true) {
+				Frog.update();
+				if(treeCurrent!=0 && Frog.y ==40){
+					isGameOver = true;
+					System.out.println("Game Over");
+				}
+				for (int i = 0; i < 3; i++) {
 					trees[i].update();
-				if(treeCurrent<3)
-					if(Frog.isCollide(trees[treeCurrent])){
-						System.out.println("Collision");
-						treeCurrent++;
-					}
-					
+					if (treeCurrent < 3)
+						if (Frog.isCollide(trees[treeCurrent])) {
+							System.out.println("Collision");
+							treeCurrent++;
+						}
+
 				}
 			}
 		}
-		
-		if (isGameOver){
+
+		if (isGameOver) {
 			Input input1 = container.getInput();
-			if(input1.isKeyDown(Input.KEY_ENTER)){
+			if (input1.isKeyDown(Input.KEY_ENTER)) {
 				init(container);
-				isGameOver = false ;
-				
+				isGameOver = false;
+
 			}
 		}
-		
+
 	}
-	
+
 	public void keyPressed(int key, char c) {
-		
-		if (key == Input.KEY_ENTER){
+
+		if (key == Input.KEY_ENTER) {
 			isStarted = true;
 		}
-			if (key == Input.KEY_SPACE) {	
-				Frog.jump();
+		if (key == Input.KEY_SPACE) {
+			Frog.jump();
 		}
-			
-			if (isGameOver){
-				if(key == Input.KEY_ENTER){
-					isGameOver = true;
-				}
-				
+
+		if (isGameOver) {
+			if (key == Input.KEY_ENTER) {
+				isGameOver = true;
 			}
-	    }
-	
-	
+
+		}
+	}
+
 	public static void main(String[] args) {
-	    try {
-	      Thefrog game = new Thefrog("THE FROG");
-	      AppGameContainer appgc = new AppGameContainer(game);
-	      appgc.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
-	      appgc.setMinimumLogicUpdateInterval(1000 / 60);
-	      appgc.start();
-	    } catch (SlickException e) {
-	      e.printStackTrace();
-	    }
-	  }
+		try {
+			Thefrog game = new Thefrog("THE FROG");
+			AppGameContainer appgc = new AppGameContainer(game);
+			appgc.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
+			appgc.setMinimumLogicUpdateInterval(1000 / 60);
+			appgc.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/thefrog/frog.java
+++ src/thefrog/frog.java
@@ -4,11 +4,11 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class frog {
-	
+
 	public float x;
-	public  float y;
+	public float y;
 	private float vjump;
-	public  float vy;
+	public float vy;
 	public float vx;
 	int score = 0;
 	private float xfrog;
@@ -18,57 +18,54 @@ public class frog {
 	public static final int HEIGHT = 60;
 	private float g;
 
-	public frog(float x, float y ,float vjump) throws SlickException {
+	public frog(float x, float y, float vjump) throws SlickException {
 		this.x = x;
 		this.y = y;
 		this.vy = vjump;
 		this.vjump = vjump;
-	    this.vx=0;
-	    g=Thefrog.G;
+		this.vx = 0;
+		g = Thefrog.G;
 		image = new Image("res/cut.png");
 	}
-	
-	
 
 	public void render() {
-		image.draw(x - WIDTH/2, 
-				Thefrog.GAME_HEIGHT - y - (HEIGHT/2));
-		
+		image.draw(x - WIDTH / 2, Thefrog.GAME_HEIGHT - y - (HEIGHT / 2));
+
 	}
-	
+
 	public void update() {
 		y += vy;
-		vy-=g;
-		 if(x > 640 - WIDTH){
-		    	vx*=-1;
-		    	
-		    }
-		    else if(x<60)
-		    	vx*=-1;
-	    x-=this.vx;
-	    if(y < 40 ){
-	    	y = 40;
-	    }
+		vy -= g;
+		if (x > 640 - WIDTH) {
+			vx *= -1;
+
+		} else if (x < 60)
+			vx *= -1;
+		x -= this.vx;
+		if (y < 40) {
+			y = 40;
+		}
 		
 	}
-	public void jump() { 
-	    vy = vjump;
-	    g=Thefrog.G;
-	  }	
 
-	public boolean isCollide(tree t){
-		if (Math.abs(y-t.getY())<=HEIGHT){
-			if(Math.abs( (x+WIDTH/2) - (t.getX()+tree.WIDTH/2) ) <= tree.WIDTH){
-				this.vy=0;
-				x= t.getX()+tree.WIDTH/2 ;
-				y= t.getY()+tree.HEIGHT/2-tree.HEIGHT/2;
-				this.g=0;
-				this.vx=t.getVx();
+	public void jump() {
+		vy = vjump;
+		g = Thefrog.G;
+	}
+
+	public boolean isCollide(tree t) {
+		if (Math.abs(y - t.getY()) <= HEIGHT) {
+			if (Math.abs((x + WIDTH / 2) - (t.getX() + tree.WIDTH / 2)) <= tree.WIDTH) {
+				this.vy = 0;
+				x = t.getX() + tree.WIDTH / 2;
+				y = t.getY() + tree.HEIGHT / 2 - tree.HEIGHT / 2;
+				this.g = 0;
+				this.vx = t.getVx();
 				score++;
 				return true;
-				
+
 			}
 		}
-		return false ; 
+		return false;
 	}
 }
